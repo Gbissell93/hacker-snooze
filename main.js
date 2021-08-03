cae9b4d27774a838d31cd8289f62526c861fe723
Gbissell93 <gregorybisselledu@gmail.com>
2021-08-03 11:22:45 -0400

    fixed loop error

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //target dom elements
 const stories = document.querySelector('#stories');
 const container = document.querySelector('.container')
-let top100 = []
 //fetch data 
 const stuff = fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
 .then((rawData) => {
@@ -15,8 +14,6 @@ const stuff = fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print
            return reponse.json()
        })
        .then((data) => {
-           top100.push(data)
-           top100.forEach((item) => {
                //create li
             const li = document.createElement('li');
             li.setAttribute('class', 'list-group-item');
@@ -24,20 +21,21 @@ const stuff = fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print
             const aTag =document.createElement('a');
             aTag.setAttribute('href', data.url);
             aTag.setAttribute('class', 'text-decoration-none');
-            aTag.setAttribute('style', 'color:#black;');
+            aTag.setAttribute('style', 'color:#0CA5AD;');
             //create div
             const title = document.createElement('h3')
             title.innerHTML = data.title
             const div = document.createElement('div');
             div.setAttribute('class', 'small light')
-            div.innerHTML = `${data.score}  by ${data.by}  comments ${data.descendants} `
+            div.setAttribute('style', ' color:light gray;')
+            div.innerHTML = `Score ${data.score} Submutted by ${data.by}  Comments ${data.descendants} `
 
             aTag.appendChild(title)
             li.appendChild(aTag)
             li.appendChild(div)
             stories.appendChild(li)
 
-           })
+           
        })
     }
 })
